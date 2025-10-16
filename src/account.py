51acc28ffc4c9ba59792a89a557c536fc06059e6
mikolajroot <mikolaj150878@gmail.com>
2025-10-16 15:47:06 +0200

    added promo code

--- src/account.py
+++ src/account.py
@@ -1,8 +1,14 @@
 class Account:
-    def __init__(self, first_name, last_name,pesel):
+    def __init__(self, first_name, last_name,pesel,promoCode=None):
         self.first_name = first_name
         self.last_name = last_name
-        self.balance = 0.0
-        self.pesel = pesel
-        if len(pesel) != 11:
-            self.pesel = 'invalid'
+        self.pesel = pesel if self.is_pesel_valid(pesel) else 'Invalid'
+        self.balance = 50.0 if self.is_promoCode_valid(promoCode) else 0
+
+    def is_pesel_valid(self,pesel):
+        if isinstance(pesel,str) and len(pesel) == 11:
+            return True
+
+    def is_promoCode_valid(self,promoCode):
+        if promoCode is not None and promoCode.startswith('PROM_') and len(promoCode) == 8:
+            return True
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -8,4 +8,32 @@ class TestAccount:
         assert account.last_name == "Doe"
         assert account.balance == 0.0
         assert account.pesel == 'invalid'
-        assert len(account.pesel) == 11 or account.pesel == 'invalid'
+        assert account.kod is None
+
+    def test_pesel_too_long(self):
+        account = Account("Jane","Doe",'4234324342424234')
+        assert account.pesel == 'invalid'
+
+    def test_pesel_too_short(self):
+        account = Account("Jane","Doe",'871368')
+        assert account.pesel == 'invalid'
+
+    def test_pesel_empty(self):
+        account = Account("Jane", "Doe", '')
+        assert account.pesel == 'invalid'
+
+    def test_correct_promoCode(self):
+        account = Account("Jane", "Doe", '','PROM_123')
+        assert account.balance == 50.0
+
+    def test_too_long_promoCode(self):
+        account = Account("Jane", "Doe", '','PROM_1234')
+        assert account.balance == 0.0
+
+    def test_too_short_promoCode(self):
+        account = Account("Jane", "Doe", '','PROM_12')
+        assert account.balance == 0.0
+
+    def test_bad_promoCode(self):
+        account = Account("Jane", "Doe", '','PROM-123')
+        assert account.balance == 0.0

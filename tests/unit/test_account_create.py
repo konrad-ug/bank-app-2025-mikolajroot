98ea27e20ad77401cd5e0144e868ca1eefffd182
mikolajroot <mikolaj150878@gmail.com>
2025-10-16 15:16:19 +0200

    added checking pesel length

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,8 +3,9 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe",134321346541)
+        account = Account("John", "Doe",'13432134654')
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0.0
-        assert account.pesel == 134321346541
+        assert account.pesel == '13432134654'
+        assert len(account.pesel) == 11

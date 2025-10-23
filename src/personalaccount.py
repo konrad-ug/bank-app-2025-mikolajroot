3c4c970c73fd15ae486545512a4d893c611a0941
mikolajroot <mikolaj150878@gmail.com>
2025-10-23 15:34:06 +0200

    added lab3

---
+++ src/personalaccount.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+
+
+class Account:
+    def __init__(self,balance):
+        self.balance = 0.0
+
+    def transfer_in(self,money: float) -> None:
+        if money > 0:
+            self.balance += money
+
+    def transfer_out(self,money: float) -> None:
+        if 0 < money <= self.balance:
+            self.balance -= money
+
+
+class PersonalAccount(Account):
+    def __init__(self, first_name, last_name,pesel,promoCode=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pesel = pesel if self.is_pesel_valid(pesel) else 'Invalid'
+        self.balance = 50.0 if self.is_promoCode_valid(promoCode) and self.is_person_age_good(pesel) else 0
+
+    def is_pesel_valid(self,pesel):
+        if isinstance(pesel,str) and len(pesel) == 11:
+            return True
+
+    def is_promoCode_valid(self,promoCode):
+        if promoCode is not None and promoCode.startswith('PROM_') and len(promoCode) == 8:
+            return True
+    def is_person_age_good(self,pesel):
+        if pesel != "Invalid":
+            if  25 > int(pesel[0:2]) or int(pesel[0:2]) > 60 :
+                return True
+
+    def express_transfer_out(self,money):
+        if 0 < money <= self.balance + 1.0:
+            self.balance -= money + 1.0
+
+
+class CompanyAccount(Account):
+    def __init__(self, company_name,nip):
+        self.company_name = company_name
+        self.nip = nip if self.is_nip_valid(nip) else 'Invalid'
+
+    def is_nip_valid(self,pesel):
+        if isinstance(pesel,str) and len(pesel) == 10:
+            return True
+
+    def express_transfer_out(self,money):
+        if 0 < money <= self.balance + 5.0:
+            self.balance -= money + 5.0
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -1,47 +1,160 @@
-from src.account import Account
-
+from src.personalaccount import PersonalAccount
+from src.personalaccount import CompanyAccount
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe",'134321346542')
+        account = PersonalAccount("John", "Doe", '134321346542')
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0.0
         assert account.pesel == 'Invalid'
 
     def test_pesel_too_long(self):
-        account = Account("Jane","Doe",'4234324342424234')
+        account = PersonalAccount("Jane", "Doe", '4234324342424234')
         assert account.pesel == 'Invalid'
 
     def test_pesel_too_short(self):
-        account = Account("Jane","Doe",'871368')
+        account = PersonalAccount("Jane", "Doe", '871368')
         assert account.pesel == 'Invalid'
 
     def test_pesel_empty(self):
-        account = Account("Jane", "Doe", '')
+        account = PersonalAccount("Jane", "Doe", '')
         assert account.pesel == 'Invalid'
 
-    def test_correct_promoCode(self):
-        account = Account("Jane", "Doe", '','PROM_123')
-        assert account.balance == 50.0
 
     def test_too_long_promoCode(self):
-        account = Account("Jane", "Doe", '','PROM_1234')
+        account = PersonalAccount("Jane", "Doe", '', 'PROM_1234')
         assert account.balance == 0.0
 
     def test_too_short_promoCode(self):
-        account = Account("Jane", "Doe", '','PROM_12')
+        account = PersonalAccount("Jane", "Doe", '', 'PROM_12')
         assert account.balance == 0.0
 
     def test_bad_promoCode(self):
-        account = Account("Jane", "Doe", '','PROM-123')
+        account = PersonalAccount("Jane", "Doe", '', 'PROM-123')
         assert account.balance == 0.0
 
     def test_to_old(self):
-        account = Account("Jane", "Doe", '55232465786', 'PROM_123')
+        account = PersonalAccount("Jane", "Doe", '55232465786', 'PROM_123')
         assert account.balance == 0.0
 
     def test_young(self):
-        account = Account("Jane", "Doe", '62232465786', 'PROM-123')
+        account = PersonalAccount("Jane", "Doe", '62232465786', 'PROM_123')
         assert account.balance == 50.0
 
+    def test_transfer_in(self):
+        account = PersonalAccount("Jane", "Doe", '62232465786', 'PROM_123')
+        assert account.balance == 50.0
+        account.transfer_in(50.0)
+        assert account.balance == 100.0
+
+    def test_correct_transfer_out(self):
+        account = PersonalAccount("Jane", "Doe", '62232465786', 'PROM_123')
+        assert account.balance == 50.0
+        account.transfer_out(20.0)
+        assert account.balance == 30.0
+
+    def test_too_low_balance_transfer_out(self):
+        account = PersonalAccount("Jane", "Doe", '62232465786', 'PROM-123')
+        assert account.balance == 0
+        account.transfer_out(20.0)
+        assert account.balance == 0
+
+    def test_express_transfer_out(self):
+        account = PersonalAccount("Jane", "Doe", '62232465786', 'PROM-123')
+        account.balance = 50.0
+
+        account.express_transfer_out(30.0)
+
+        assert account.balance == 19.0
+
+    def test_express_negative_transfer_out(self):
+        account = PersonalAccount("Jane", "Doe", '62232465786', 'PROM-123')
+        account.balance = 50.0
+
+        account.express_transfer_out(50.0)
+
+        assert account.balance == -1.0
+
+    def test_express_too_low_balance_transfer_out(self):
+        account = PersonalAccount("Jane", "Doe", '62232465786', 'PROM-123')
+        account.balance = 50.0
+
+        account.express_transfer_out(60)
+
+        assert account.balance == 50.0
+
+
+class TestCompanyAccount:
+    def test_account_creation(self):
+        account = CompanyAccount('nazwa_firmy', '1343213465')
+        assert account.company_name == 'nazwa_firmy'
+        assert account.nip == '1343213465'
+    def test_nip_too_long(self):
+        account = CompanyAccount('nazwa_firmy', '13432134654')
+        assert account.nip == 'Invalid'
+
+    def test_nip_too_short(self):
+        account = CompanyAccount('nazwa_firmy', '1343')
+        assert account.nip == 'Invalid'
+
+    def test_nip_empty(self):
+        account = CompanyAccount('nazwa_firmy', '')
+        assert account.nip == 'Invalid'
+
+
+    def test_transfer_in(self):
+        account = CompanyAccount('nazwa_firmy', '1343213465')
+        account.balance = 50.0
+
+        account.transfer_in(100.0)
+
+        assert account.balance == 150.0
+
+    def test_correct_transfer_out(self):
+        account = CompanyAccount('nazwa_firmy', '1343213465')
+        account.balance = 50.0
+
+        account.transfer_out(20.0)
+
+        assert account.balance == 30.0
+
+    def test_too_low_balance_transfer_out(self):
+        account = CompanyAccount('nazwa_firmy', '1343213465')
+        account.balance = 50.0
+
+        account.transfer_out(100.0)
+
+        assert account.balance == 50.0
+
+    def test_negative_number_transfer_out(self):
+        account = CompanyAccount('nazwa_firmy', '1343213465')
+        account.balance = 50.0
+
+        account.transfer_out(-30.0)
+
+        assert account.balance == 50.0
+
+    def test_express_transfer_out(self):
+        account = CompanyAccount('nazwa_firmy', '1343213465')
+        account.balance = 50.0
+
+        account.express_transfer_out(30.0)
+
+        assert account.balance == 15.0
+
+    def test_express_negative_transfer_out(self):
+        account = CompanyAccount('nazwa_firmy', '1343213465')
+        account.balance = 50.0
+
+        account.express_transfer_out(50.0)
+
+        assert account.balance == -5.0
+
+    def test_express_too_low_balance_transfer_out(self):
+        account = CompanyAccount('nazwa_firmy', '1343213465')
+        account.balance = 50.0
+
+        account.express_transfer_out(60)
+
+        assert account.balance == 50.0
